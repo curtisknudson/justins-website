2dce559fdfa0277d2bdb4d0064fc1835a6524090
Curtis Knudson <curtisknudson@pm.me>
2024-05-26 18:40:39 -0600

    Justin size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex justify-center items-center mt-24">
-      Justin has a ......
+    <main className="flex justify-center items-center mt-24 text-2xl">
+      Justin has a massive weiner!!!!
     </main>
   );
 }
